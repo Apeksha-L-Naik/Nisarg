63f6085e73dd07501d70f90c6434d1daf062a152
Kshamas123 <kshamas618@gmail.com>
2024-09-15 00:06:15 +0530

    made some changes to login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login';
+import Signup from './Components/Signup'; // Adjust the import path according to your project structure
+
 function App() {
   return (
-    <div className="App">
-     <Login /> 
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* Add other routes here as needed */}
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './Login.css'; // Import the CSS file
 import leafImage from '../assets/leaf2.jpg'; // Background image path
+import { Link } from 'react-router-dom'; // Import Link
 
 function Login() {
   const customButtonStyle = {
@@ -20,28 +21,31 @@ function Login() {
           <img src={nisargLogo} alt="App Logo" width="300" /> {/* Adjust logo size as needed */}
           <h1>Login</h1>
           <p>Welcome to our App</p>
-          <a href="#" className="register-link">Register</a>
         </div>
 
         <Form className="login-form">
           <Form.Group controlId="formEmail">
-            <Form.Label>Email</Form.Label>
+            <Form.Label className="text-content">Email</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
           </Form.Group>
 
           <Form.Group controlId="formPassword">
-            <Form.Label>Password</Form.Label>
+            <Form.Label className="text-content">Password</Form.Label>
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
 
           <Form.Group controlId="formCheckbox">
             <Form.Check type="checkbox" label="Remember me" />
           </Form.Group>
-
+          
           <Button variant="custom" style={customButtonStyle} type="submit" className="login-btn">
             Login
           </Button>
         </Form>
+
+        <p className="signup-link">
+          Don't have an account? <Link to="/signup">Sign Up</Link>
+        </p>
       </div>
 
       {/* Right side with background image */}
